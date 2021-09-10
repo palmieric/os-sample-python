2ed21e214db28b749e5374fa26dd81db3d25f659
Carlo Palmieri <cpalmieri@redhat.com>
2021-09-10 12:37:50 +0200

    added text/plain endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import Response
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route("/text/plain")
+def text_plain():
+    return Response("Hello World!", mimetype='text/plain')
 
 if __name__ == "__main__":
     application.run()
